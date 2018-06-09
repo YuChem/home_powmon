bae1fe6446250feeddbc62f6820bea46310806c9
YuChem <yuchem@hotmail.com>
2018-06-08 20:57:19 -0400

    emoncms url

--- powmon.py
+++ powmon.py
@@ -6,14 +6,14 @@
 '''
 import requests
 from sensors.pzem import Pzem_004
-EMONCMS_URL = "http:192.168.1.19/input/post?node=emontx&json={{current:{A},voltage:{V},power:{W},energy{Wh}}"
+EMONCMS_URL = "http://192.168.1.19/emoncms/input/post?node=main&json={{current:{A},voltage:{V},power:{W},energy:{Wh}}}"
 
 if __name__ == "__main__":
     p = Pzem_004()
     p.open()
     data = p.read_all()
-
-    url = EMONCMS_URL.format(data)
+    print("sensor %s" % data)
+    url = EMONCMS_URL.format(**data)
     print("Sending %s" % url)
 
     auth = {"Authorization":"Bearer 6c9ee2ec382c16d0e9502a4da215fc7d "}
